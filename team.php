a05707a959a29c006936b9e55d70b5ff241250db
mr_sagar <mr_sagar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-04 08:34:40 +0000

    updated
    git-svn-id: https://plugins.svn.wordpress.org/team-member-showcase/trunk@1212392 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- team.php
+++ team.php
@@ -1,11 +1,11 @@
 <?php
 /*
-Plugin Name: Team Member Showcase 
+Plugin Name: Team Member Showcase pro
 Plugin URI: http://wpexpert24.com/
 Description: Team Member Showcase is a super modern Team Member wordpress plugin. With this plugin you can display a set of pictures and information in different layouts. The main purpose of the plugin is to display team/staff members. Unlimited Colors, 100% responsive, automatic resize images, flexible, unlimited items, shortcode powered, custom link and more.
 Author: RAKIBUR RAHMAN SAGAR
 Author URI: http://wpexpert24.com/
-Version: 1.0
+Version: 1.1
 */
 
 //Loading CSS
@@ -13,6 +13,7 @@ function rd_team_member_effects_style() {
 	wp_enqueue_style('team_icon_css', plugins_url( '/css/icon_all.css' , __FILE__ ) );
 	wp_enqueue_style('team_mainstyle_css', plugins_url( '/css/team_main_style.css' , __FILE__ ) );
 	wp_enqueue_style('team_styleone_css', plugins_url( '/css/team_style_one.css' , __FILE__ ) );
+	wp_enqueue_style('team_styleone_css', plugins_url( '/css/font-awesome.min.css' , __FILE__ ) );
 }
 add_action( 'wp_enqueue_scripts', 'rd_team_member_effects_style' );
 
@@ -84,155 +85,7 @@ new VP_Metabox(array
 ));
 
 
-// Register Shortcode
-function rd_team_member_shortcode($atts){
-	extract( shortcode_atts( array(
-
-		'category' => '',	
-		'style' => '',		
-		'width' => '',	
-		'height' => '',	
-		'icon_one' => '',	
-		'icon_one_link' => '',	
-		'icon_one_hover_color' => '',	
-		'icon_two' => '',	
-		'icon_two_link' => '',	
-		'icon_two_hover_color' => '',	
-		'icon_three' => '',	
-		'icon_three_link' => '',	
-		'icon_three_hover_color' => '',	
-		
-	), $atts) );
-	
-	
-	    $q = new WP_Query(
-        array('posts_per_page' => -1, 'post_type' => 'rd_team_member', 'member_cat' => $category)
-        );
-	
-		while($q->have_posts()) : $q->the_post();
-		$id = get_the_ID();	
-		
-	
-		$infos = vp_metabox('infos.rd_team_member', false);
-		
-	$i = 0;
-	
-		$output = '
-		<div class="team_content">
-		<div class="section paddinf-off rd_team">
-		<section class="rd_container">
-        <div class="rd_raw">';
-		
-		foreach ($infos as $info) {	
-
-		if ($style==style1) {
-		
-		$output .= '
-				<style type="text/css">
-					.rd_team .rd_item_square .social-icons li:nth-child(1):hover a {
-						color: #5d4bf2;
-					}
-					.rd_team .rd_item_square .social-icons li:nth-child(2):hover a {
-						color: #00b9ff;
-					}
-					.rd_team .rd_item_square .social-icons li:nth-child(3):hover a {
-						color: #da1a1a;
-					}
-					.rd_team .rd_item_square {
-					width: 216px;
-					}
-					.rd_team .rd_item_square .face {
-						height: 260px; 
-					}
-				</style>
-				
-					<div class="rd_item_square">
-                        <div class="rd_face_container">
-                            <div class="face"><img alt="" src="'.$info['imgup_1'].'">
-                            </div>
-                        </div>
-                        <div class="rd_single_item-content">
-                            <div class="title">
-                                <h3>'.$info['member_label'].'</h3>
-                                <h4>'.$info['mamber_name'].'</h4>
-                            </div>
-                            <div class="text">
-                                <p>'.$info['desc'].'</p>
-                            </div>
-                            <div class="footer">
-                                <ul class="social-icons">
-                                    <li><a href="'.$icon_one_link.'"><i class="icon-facebook"></i></a>
-                                    </li>
-                                    <li><a href="'.$icon_two_link.'"><i class="icon-twitter"></i></a>
-                                    </li>
-                                    <li><a href="'.$icon_three_link.'"><i class="icon-youtube"></i></a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-				';
-		}
-		if ($style==style2) {
-		
-		$output .= '
-				<style type="text/css">
-					.rd_team .rd_item_square-2 .social-icons li:nth-child(1):hover a {
-						color: #5d4bf2;
-					}
-					.rd_team .rd_item_square-2 .social-icons li:nth-child(2):hover a {
-						color: #00b9ff;
-					}
-					.rd_team .rd_item_square-2 .social-icons li:nth-child(3):hover a {
-						color: #da1a1a;
-					}
-					.rd_team .rd_item_square-2 {
-					width: 216px;
-					}
-					.rd_team .rd_item_square-2 .face {
-						height: 260px; 
-					}
-				</style>
-                    <div class="rd_item_square-2">
-                        <div class="title">
-                            <h3>'.$info['mamber_name'].'</h3>
-                        </div>
-						<div class="rd_face_container">
-                            <div class="face"><img alt="" src="'.$info['imgup_1'].'">
-                            </div>
-                        </div>
-                        <div class="rd_single_item-content">
-                            <div class="footer">
-                                <h4>'.$info['member_label'].'</h4>
-									 <p>'.$info['desc'].'</p>
-                                <ul class="social-icons">
-                                    <li><a href="'.$icon_one_link.'"><i class="icon-facebook"></i></a>
-                                    </li>
-                                    <li><a href="'.$icon_two_link.'"><i class="icon-twitter"></i></a>
-                                    </li>
-                                    <li><a href="'.$icon_three_link.'"><i class="icon-youtube"></i></a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                        </div>
-				';
-		}
-		$i++;
-	}
-	
-	endwhile;
-	$output .='
-	</div>
-    </section>
-	</div>
-	</div>';
-	wp_reset_query();
-	return $output;
-}
-
-add_shortcode('member', 'rd_team_member_shortcode');
-
+require 'backend/shortcode.php';
 
 
 //Shortcode Generator 
